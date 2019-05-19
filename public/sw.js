879fb48f2465c9e9285ea176f65de7168210def3
vai91 <mehmetefeekiner@gmail.com>
2019-05-19 22:11:44 +0200

    Cache multiple files with cacheAll

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,25 @@
+var CACHED_URLS = [
+  '/',
+  '/index.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  // polyfills do not need to be cached, since the older browsers do not support serviceworkers anyway. But, there's performance gain. 
+  // Even modern browsers need to load these files, because they imported in index.html, so storing them in cache will increase performance.
+  // This can be optimized with a build workflow, to conditionally load them
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  // Styles
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+  '',
+  // Images
+  '/src/images/main-image.jpg'
+]
+
 self.addEventListener("install", function (event) {
   console.log("[ServiceWorker] Installing ServiceWorker ...", event);
   //ensuring installation event is not completed before caching is done.
@@ -6,9 +28,7 @@ self.addEventListener("install", function (event) {
       .then(function (cache) {
         console.log('[ServiceWorker] Precaching App shell');
         /*sw will send the request to the server, get that asset, and stores it, in one step. */
-        cache.add('/');
-        cache.add('/index.html');
-        cache.add('/src/js/app.js');
+        cache.addAll(CACHED_URLS);
       })
   );
 });
