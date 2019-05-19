0d6970861b3bf20dece4c81b41bac28442d4dd7a
vai91 <mehmetefeekiner@gmail.com>
2019-05-19 12:10:40 +0200

    Static Caching / Precaching

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,14 @@
 self.addEventListener("install", function (event) {
   console.log("[ServiceWorker] Installing ServiceWorker ...", event);
+  //ensuring installation event is not completed before caching is done.
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        console.log('[ServiceWorker] Precaching App shell');
+        /*sw will send the request to the server, get that asset, and stores it, in one step. */
+        cache.add('src/js/app.js');
+      })
+  );
 });
 
 self.addEventListener("activate", function (event) {
