0051a2d892c8df6e8daed81f4c0b41a8e236c43d
vai91 <mehmetefeekiner@gmail.com>
2019-05-21 01:05:49 +0200

    Re-improvement of parsing Static Cache URLs

--- public/sw.js
+++ public/sw.js
@@ -57,12 +57,14 @@ self.addEventListener("activate", function (event) {
 });
 
 function isInArray(string, array) {
-  for (let index = 0; index < array.length; index++) {
-    if (array[index] === string) {
-      return true;
-    }
+  var cachePath;
+  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log('matched ', string);
+    cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
+  } else {
+    cachePath = string; // store the full request (for CDNs)
   }
-  return false;
+  return array.indexOf(cachePath) > -1;
 }
 
 self.addEventListener('fetch', function (event) {
